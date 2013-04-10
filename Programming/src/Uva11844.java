fee255112cd19447f86d437b7d033f73b0b1fda6
Milena <Milena@10.10.0.142>
2013-04-10 17:12:33 -0500

    YA casi... arreglar mutar(vi)

--- Programming/src/Uva11844.java
+++ Programming/src/Uva11844.java
@@ -1,38 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Map.Entry;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
-
+import java.io.*;
+import java.util.*;
 
 public class Uva11844 {
 	static TreeMap<String, String> mu ; 
 	static TreeMap<String, Integer> vi ;
 	static TreeMap<String, Integer> cu ;
-	static ArrayList<String> compartidas;
 	public static void main(String[] args) throws Throwable {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder( );
 		for (String line; (line = in.readLine()) !=null; ) {
 			StringTokenizer st = new StringTokenizer(line);
 			int m = Integer.parseInt(st.nextToken());
 			int i = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int n = Integer.parseInt(st.nextToken());
-			boolean hayCura = true;
 			if( m == 0 && i==0 && c == 0 && n == 0  ) break;
 
+			boolean isCure = false;
+			boolean isDeterministic = true;
 			mu = new TreeMap<String, String>();
 			vi = new TreeMap<String, Integer>();
 			cu = new TreeMap<String, Integer>();
-			compartidas = new ArrayList<String>();
-			for (int mutaciones = 0; mutaciones < m && hayCura; mutaciones++) {
+			
+			for (int mutaciones = 0; mutaciones < m ; mutaciones++) {
 				st = new StringTokenizer(in.readLine());
 				String key = st.nextToken( );
 				String val = st.nextToken( );
 				if( mu.containsKey(key) ){
 					if( !mu.get(key).equals(val) ){
-						hayCura = false;
+						isDeterministic = false;
 					}
 				}else{
 					mu.put(key, val);
@@ -54,30 +50,75 @@ public class Uva11844 {
 				st = new StringTokenizer(in.readLine());
 				String key = st.nextToken( );
 				int val = Integer.parseInt(st.nextToken( ));
-				if(vi.containsKey(key)){
-					//TODO restar al virus y si llega a 0 borrar de virus
-					compartidas.add(key);
-				}
 				if( cu.containsKey(key) ){
 					cu.put(key, cu.get(key)+val);
 				}else{
 					cu.put(key, val);
 				}
 			}
+			
+//			System.out.println("Mutac: " + mu);
+//			System.out.println("Virus: " + vi);
+//			System.out.println("Curas: " + cu);
 
-			for (int j = 0; j < n; j++) {
-				
-				String key = compartidas.get(i);
-				if( cu.get(key)-vi.get(key) < 0  ){
-					
+			
+			int k = 0;
+			if( isDeterministic ){
+				for (int j = 0; j < n && !isCure; j++) {
+					if( iguales(vi, cu) ){
+						isCure = true;
+					}else{
+						System.out.println("Muta de: " + vi);
+						mutar(vi);
+						System.out.println("Muta a : " + vi );
+					}
+					k = j;
 				}
 			}
 
-
-
-			System.out.println( vi );
-			System.out.println( cu );
-
+			if( !isDeterministic ){
+				sb.append("Protein mutations are not deterministic");
+			}else{
+				if(!isCure)
+					sb.append("Nostalgia for Infinity is doomed");
+				else
+					sb.append("Cure found in "+k+" mutation(s)");
+			}
+			sb.append("\n");
 		}
+		System.out.print(new String(sb));
+	}
+
+	public static void mutar( TreeMap<String, Integer> vi ) {
+		ArrayList<String> keysToDelete = new ArrayList<String>();
+		ArrayList<String> keysToAdd = new ArrayList<String>();
+		for (String key : vi.keySet() ) {
+			if( mu.containsKey( key ) ){
+				keysToAdd.add(mu.get(key));
+				keysToDelete.add( key );
+			}
+		}
+		for (int i = 0; i < keysToDelete.size(); i++) {
+			String keyDel  =  keysToDelete.get(i);
+			String keyAdd  =  keysToAdd.get(i);
+			System.out.println( keyDel + "  " + keyAdd );
+ 			if( vi.containsKey(keyAdd) ){
+				vi.put(keyAdd, vi.get(keyAdd)+vi.get(keyDel));
+			}else{
+				vi.put(keyAdd,vi.get(keyDel));
+			}
+ 			vi.remove(keyDel);
+		}
+	}
+	
+	public static boolean iguales( TreeMap<String, Integer> vi , TreeMap<String, Integer> cu ){
+		boolean iguales = true;
+		for (String key : cu.keySet() ) {
+			if( vi.containsKey(key) && vi.get(key) != cu.get(key) )
+				iguales = false;
+			else if( !vi.containsKey(key) )
+				iguales = false;
+		}
+		return iguales;
 	}
 }
