94dfc1919b26e33aa98178080a6b927937f4a631
Angie Milena Vega Leon <angie.vega@helisa.com>
2013-04-18 14:21:16 -0500

    peter smokes

---
+++ Programming/src/Uva10346.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Uva10346 {
+
+	public static void main(String[] args) throws Throwable {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder( );
+		for (String line; (line = in.readLine())!=null ; ) {
+			StringTokenizer st = new StringTokenizer(line);
+			long n = Integer.parseInt(st.nextToken());
+			long k = Integer.parseInt(st.nextToken());
+			sb.append(solve(n,k)+"\n");
+		}
+		System.out.print(new String(sb));
+	}
+	
+	private static long solve(long n, long k) {
+		long sum = n;
+		while(  n >= k  ) {
+			sum+=( n=(long) Math.floor(n/k) );
+		}
+		return sum;
+	}
+}
+
+
+//private static long solve(long n, long k ){
+//long cont = n;
+//long col = n;
+//while(col >= k){
+//	cont++;
+//	col-=k;
+//	col++;
+//}
+//return cont;
+//}
