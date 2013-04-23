06a9566e00012c54c03529718c10afd9b32ab51a
Angie Milena Vega Leon <angie.vega@helisa.com>
2013-04-23 12:56:55 -0500

    Power Crisis Acc

--- Programming/src/Uva151.java
+++ Programming/src/Uva151.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 
 public class Uva151 {
@@ -11,28 +12,33 @@ public class Uva151 {
 			int n = Integer.parseInt(line);
 			if(n == 0)break;
 			off=new boolean[n];
-			int k = 5;
-			int cpy = 5;
-			off[0]=true;
-			for (int i = 0; !go( ) ; i++ ) {
-				if(i>=n)i=i%8;
-				if((cpy == 0)|| i==0) {
-					System.out.println(i+1);
-					off[i]=true;
-					cpy=k;
-				}else if(!off[i]|| i==0){
-					cpy--;
+
+			boolean termine = false;
+			for (int j = 1; !termine; j++) {
+				Arrays.fill(off, false);
+				int total = 0;
+				int m = j;
+				int cpy = 0;
+				int ultimoApagado=-1;
+				for (int i = 0; total < n ; i++) {
+					if(i >= n )
+						i = 0;
+					if( cpy == 0 && !off[i] ){
+//						System.out.println(i+1);
+						ultimoApagado = i+1;
+						off[i] = true;
+						cpy = m-1;
+						total++;
+					}else if(!off[i]){
+						cpy--;
+					}
+				}
+//				System.out.println("aaaaaaaaa " + total+ " "+ultimoApagado);
+				if(ultimoApagado == 13){
+					termine = true;
+					System.out.println(m);
 				}
 			}
 		}
 	}
-	
-	public static boolean go( ){
-		for (int i = 0; i < off.length; i++) {
-			if( !off[i] )
-				return false;
-		}
-		return true;
-	}
-
 }
