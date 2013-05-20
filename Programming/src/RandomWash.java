03c8ab8bc7dfa0c0af8059b98cddf664bbd40e75
Milena <Milena@192.168.1.189>
2013-05-20 17:27:42 -0500

    TOLIS

--- Programming/src/RandomWash.java
+++ Programming/src/RandomWash.java
@@ -7,7 +7,8 @@ import java.util.Random;
 
 public class RandomWash {
 
-
+	private static final long WAIT = 500;
+	static ArrayList<String> perdedores;
 	static String MSG_SALVADO = "PEGO EN EL PALO : ";
 	static String MSG_LE_TOCO = " GOOOOLAZO DE ";
 	static int rondas = 2;
@@ -16,7 +17,7 @@ public class RandomWash {
 	static boolean vo[];
 	public static void main(String[] args) throws Throwable {
 		BufferedReader in = new BufferedReader(new FileReader("in.in"));
-
+		perdedores = new ArrayList<String>();
 		od1enses = new ArrayList<String>();
 		for (String line; (line=in.readLine())!=null;) {
 			od1enses.add(line);
@@ -27,34 +28,38 @@ public class RandomWash {
 		for (int i = 0; i < rondas; i++) {
 			System.out.println("-------------------");
 			System.out.println("      RONDA "+(i+1) );
-//			Arrays.fill(vo, false);
-			for (int j = 0; j < oport; j++) {
-				int posOd1=min + (int)(Math.random() * ((max - min) + 1));
-				if( !vo[posOd1] ){
-					if( j<oport-1 ){
+			Arrays.fill(vo, false);			
+			for (int j = 0; j <= oport; ) {
+				int posOd1 = min + (int)(Math.random() * ((max - min) + 1));
+				if( !vo[posOd1] && !perdedores.contains(od1enses.get(posOd1))){
+					if( j<oport ){
 						System.out.print(MSG_SALVADO);
 						suspensivos();
 						System.out.println(od1enses.get(posOd1));
-						
 					}else{
 						System.out.print(MSG_LE_TOCO);
 						suspensivos();
 						System.out.println(od1enses.get(posOd1));
-//						vo[posOd1]=true;
+						perdedores.add(od1enses.get(posOd1));
 					}
+					j++;
 					vo[posOd1]=true;
-				}else{
-					j-=2;
 				}
 			}
 		}
+		System.out.println("\n-------------------");
+		System.out.println("     PERDEDORES    " );
+		for (int i = 0; i < perdedores.size(); i++) {
+			System.out.println(perdedores.get(i));
+		}
+		System.out.println("-------------------");
 	}
 	
 	public static void suspensivos( ){
 		try {
 			for (int i = 0; i < 3; i++) {
 				System.out.print(". ");
-				Thread.sleep(1000);
+				Thread.sleep(WAIT);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
