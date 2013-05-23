fd4f1783be5f8c379ec538a9fc019b89d2df0f8a
Angie Milena Vega Leon <angie.vega@helisa.com>
2013-05-23 08:29:58 -0500

    Commit

---
+++ Programming/src/TrianguloPascal.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
+
+public class TrianguloPascal {
+
+	static BigInteger pascal[][];
+	public static void main(String[] args) {
+		trianguloPascal();
+		System.out.println(pascal[16][3]);
+	}
+
+
+	static void trianguloPascal( ){
+		pascal = new BigInteger[101][102];
+		for (int i = 0; i < pascal.length; i++) 
+			Arrays.fill(pascal[i], BigInteger.valueOf(-1));
+
+		for (int i = 0; i < pascal.length; i++) {
+			boolean termine = false;
+			for (int j = 0; j < pascal[0].length && !termine; j++) {
+				if(j==0) pascal[i][j]=BigInteger.ONE;
+				else if( i-1 >= 0 && j-1 >= 0 ){
+					if(pascal[i-1][j].compareTo(BigInteger.ZERO)==1)
+						pascal[i][j] = pascal[i-1][j-1].add(pascal[i-1][j]);
+					else {
+						pascal[i][j] = BigInteger.ONE;
+						termine = true;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Programming/src/Uva10943.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Uva10943 {
+
+	static int dp[][] = new int[102][102];
+
+	static void dpM ( ){
+		
+		for (int i = 0; i < dp.length; i++) {
+			dp[0][i] = 0;
+			dp[i][0] = 1;
+			dp[1][i] = 1;
+		}
+		for (int i = 2; i < dp.length; i++) {
+			for (int j = 1; j < dp[i].length; j++) {
+				dp[i][j] = (dp[i-1][j]%1000000+dp[i][j-1]%1000000)%1000000;
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Throwable {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		dpM();
+		for (String line; (line = in.readLine())!=null; ) {
+			StringTokenizer st = new StringTokenizer(line);
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			if(n==0&&k==0)break;
+			System.out.println(dp[k][n]);
+		}
+	}
+}
