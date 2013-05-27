8820ee8eff280c7b392d1f8c965ce6a30d6d86db
Milena <Milena@MilenaV>
2013-05-26 21:02:11 -0500

    Primer Intento

--- Programming/src/Uva10293.java
+++ Programming/src/Uva10293.java
@@ -1,8 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
 
 public class Uva10293 {
 
-	public static void main(String[] args) {
+	static TreeMap<Integer, Integer> tree;
+	public static void main(String[] args) throws Throwable {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		tree = new TreeMap<Integer, Integer>();
+		int k = 0;
+		int cantLetras = 0;
+		for (String line ; (line = in.readLine())!=null; k++) {
+			line = line.trim();
+			if(line.startsWith("#")){
+				for (Integer i : tree.keySet()) {
+					sb.append(i +" "+ tree.get(i)+"\n");
+				}
+				sb.append("\n");
+				tree = new TreeMap<Integer, Integer>(); 
+			}
 
+			for (int i = 0; i < line.length(); i++) {
+				char actual = line.charAt(i);
+				if(Character.isLetter(actual)){
+					cantLetras++;
+				}else if( cantLetras != 0 && actual != '-' && actual != '\'' ){
+					if( tree.containsKey(cantLetras) )
+						tree.put(cantLetras, tree.get(cantLetras)+1);
+					else
+						tree.put(cantLetras, 1);
+					cantLetras=0;
+				}
+				if( i == line.length() - 1 && actual != '-' && cantLetras != 0){
+					if( tree.containsKey(cantLetras) )
+						tree.put(cantLetras, tree.get(cantLetras)+1);
+					else
+						tree.put(cantLetras, 1);
+					cantLetras=0;
+				}
+			}
+			
+		}
+		String n = sb.substring(0, sb.length()-1);
+		System.out.print(n);
 	}
 
 }
