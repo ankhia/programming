47f9017eaf68ea1271d30a62705ab4ccf27ac1a5
Milena <Milena@MilenaV>
2014-08-08 19:26:27 -0500

    Accepted

--- Programming/src/Uva10196.java
+++ Programming/src/Uva10196.java
@@ -1,3 +1,15 @@
+/**
+ * @author Angie Milena Vega Leon
+ * @linkDeArchivo http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=24&problem=1137&mosmsg=Submission+received+with+ID+14010747
+ * @veredict Accepted 
+ * @problemId 10196
+ * @problemName  Check the Check
+ * @judge http://uva.onlinejudge.org/
+ * @category AC
+ * @level Implementation, Ad hoc
+ * @date 08/08/2014
+ **/ 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -69,7 +81,7 @@ public class Uva10196 {
 		//rook and queen
 		boolean found = false;
 		for (int k = j; k >=0 && !found ; k--){
-			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] == '.' )
+			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] != '.' && chess[i][k] != 'K')
 				found = true;
 			if( chess[i][k] == 'r' || chess[i][k] == 'q' ) {
 				inCheck = true;
@@ -77,23 +89,23 @@ public class Uva10196 {
 		}
 		found = false;
 		for (int k = i; k >=0 && !found; k--){
-			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] == '.' )
+			if( chess[k][j] != 'r' && chess[k][j] != 'q' &&  chess[k][j] != '.' && chess[k][j] != 'K')
 				found = true;
-			if( chess[k][j] == 'r' || chess[i][k] == 'q' ) {
+			if( chess[k][j] == 'r' || chess[k][j] == 'q' ) {
 				inCheck = true;
 			}
 		}
 		found = false;
 		for (int k = i; k < 8 && !found; k++){
-			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] == '.' )
+			if( chess[k][j] != 'r' && chess[k][j] != 'q' &&  chess[k][j] != '.' && chess[k][j] != 'K')
 				found = true;
-			if( chess[k][j] == 'r' || chess[i][k] == 'q' ) {
+			if( chess[k][j] == 'r' || chess[k][j] == 'q' ) {
 				inCheck = true;
 			}
 		}
 		found = false;
-		for (int k = j; k < 8; k++){
-			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] == '.' )
+		for (int k = j; k < 8 && !found; k++){
+			if( chess[i][k] != 'r' && chess[i][k] != 'q' &&  chess[i][k] != '.' && chess[i][k] != 'K')
 				found = true;
 			if( chess[i][k] == 'r' || chess[i][k] == 'q' ){
 				inCheck =  true;
@@ -103,32 +115,31 @@ public class Uva10196 {
 		//bishop and queen
 		found = false;
 		for (int k = i, l = j ; k >=0 && l>=0 && !found; k--, l--){
-			if( chess[k][l] != 'b' && chess[i][k] != 'q' && chess[k][l] == '.')
+			if( chess[k][l] != 'b' && chess[k][l] != 'q' && chess[k][l] != '.' && chess[k][l] != 'K')
 				found = true;
-			if( chess[k][l] == 'b' || chess[i][k] == 'q' ) 
+			if( chess[k][l] == 'b' || chess[k][l] == 'q' ) 
 				inCheck = true;
 		}
 		found = false;
 		for (int k = i, l = j ; k <8 && l <8  && !found; k++, l++){
-			if( chess[k][l] != 'b' && chess[i][k] != 'q' && chess[k][l] == '.')
+			if( chess[k][l] != 'b' && chess[k][l] != 'q' && chess[k][l] != '.' && chess[k][l] != 'K')
 				found = true;
-			if( chess[k][l] == 'b' || chess[i][k] == 'q' ) {
+			if( chess[k][l] == 'b' || chess[k][l] == 'q' ) {
 				inCheck = true;
 			}
 		}
 		found=false;
-		for (int k = i, l = j ; k >=0 && l <8 && !found; k--, l++){
-			if( chess[k][l] != 'b' && chess[i][k] != 'q' && chess[k][l] == '.')
+		for (int k = i, l = j ; k >=0 && l<8 && !found; k--, l++){
+			if( chess[k][l] != 'b' && chess[k][l] != 'q' && chess[k][l] != '.' && chess[k][l] != 'K')
 				found = true;
-			if( chess[k][l] == 'b' || chess[i][k] == 'q' ){
+			if( chess[k][l] == 'b' || chess[k][l] == 'q' )
 				inCheck =  true;
-			}
 		}
 		found = false;
 		for (int k = i, l = j ; k <8 && l>=0  && !found; k++, l--){
-			if( chess[k][l] != 'b' && chess[i][k] != 'q' && chess[k][l] == '.')
+			if( chess[k][l] != 'b' && chess[k][l] != 'q' && chess[k][l] != '.' && chess[k][l] != 'K')
 				found = true;
-			if( chess[k][l] == 'b' || chess[i][k] == 'q' ) {
+			if( chess[k][l] == 'b' || chess[k][l] == 'q' ) {
 				inCheck = true;
 			}
 		}
@@ -136,7 +147,7 @@ public class Uva10196 {
 	}
 	
 	private static boolean validateBlack( ){
-		int i = wki, j = wkj ; 
+		int i = bki, j = bkj ; 
 		boolean inCheck = false ;
 		//knight
 		if( i-2 >= 0 && j-1 >= 0 && chess[i-2][j-1] == 'N' ) inCheck = true;
@@ -149,13 +160,13 @@ public class Uva10196 {
 		if( i+2  < 8 && j+1  < 8 && chess[i+2][j+1] == 'N' ) inCheck = true;
 		
 		//pawn
-		if( i+1 < 8 && j+1 < 8  && chess[i+1][j+1] == 'P') inCheck = true;
-		if( i+1 < 8 && j-1 >=0  && chess[i+1][j-1] == 'P') inCheck = true;
+		if( i+1 < 8 && j+1 <  8 && chess[i+1][j+1] == 'P') inCheck = true;
+		if( i+1 < 8 && j-1 >= 0 && chess[i+1][j-1] == 'P') inCheck = true;
 		
 		//rook and queen
 		boolean found = false;
 		for (int k = j; k >=0 && !found ; k--){
-			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] == '.' )
+			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] != '.' && chess[i][k] != 'k')
 				found = true;
 			if( chess[i][k] == 'R' || chess[i][k] == 'Q' ) {
 				inCheck = true;
@@ -163,24 +174,25 @@ public class Uva10196 {
 		}
 		found = false;
 		for (int k = i; k >=0 && !found; k--){
-			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] == '.' )
+			if( chess[k][j] != 'R' && chess[k][j] != 'Q' &&  chess[k][j] != '.' && chess[k][j] != 'k')
 				found = true;
-			if( chess[k][j] == 'R' || chess[i][k] == 'Q' ) {
+			if( chess[k][j] == 'R' || chess[k][j] == 'Q' ) {
 				inCheck = true;
 			}
 		}
 		found = false;
 		for (int k = i; k < 8 && !found; k++){
-			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] == '.' )
+			if( chess[k][j] != 'R' && chess[k][j] != 'Q' &&  chess[k][j] != '.' && chess[k][j] != 'k')
 				found = true;
-			if( chess[k][j] == 'R' || chess[i][k] == 'Q' ) {
+			if( chess[k][j] == 'R' || chess[k][j] == 'Q' ) {
 				inCheck = true;
 			}
 		}
 		found = false;
-		for (int k = j; k < 8; k++){
-			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] == '.' )
-				found = true;
+		for (int k = j; k < 8 && !found; k++){
+			if( chess[i][k] != 'R' && chess[i][k] != 'Q' &&  chess[i][k] != '.' && chess[i][k] != 'k'){
+				found = true;	
+			}
 			if( chess[i][k] == 'R' || chess[i][k] == 'Q' ){
 				inCheck =  true;
 			}
@@ -189,179 +201,37 @@ public class Uva10196 {
 		//bishop and queen
 		found = false;
 		for (int k = i, l = j ; k >=0 && l>=0 && !found; k--, l--){
-			if( chess[k][l] != 'B' && chess[i][k] != 'Q' && chess[k][l] == '.')
+			if( chess[k][l] != 'B' && chess[k][l] != 'Q' && chess[k][l] != '.' && chess[k][l] != 'k')
 				found = true;
-			if( chess[k][l] == 'B' || chess[i][k] == 'Q' ) 
+			if( chess[k][l] == 'B' || chess[k][l] == 'Q' ) 
 				inCheck = true;
 		}
 		found = false;
 		for (int k = i, l = j ; k <8 && l <8  && !found; k++, l++){
-			if( chess[k][l] != 'B' && chess[i][k] != 'Q' && chess[k][l] == '.')
+			if( chess[k][l] != 'B' && chess[k][l] != 'Q' && chess[k][l] != '.' && chess[k][l] != 'k')
 				found = true;
-			if( chess[k][l] == 'B' || chess[i][k] == 'Q' ) {
+			if( chess[k][l] == 'B' || chess[k][l] == 'Q' ) {
 				inCheck = true;
 			}
 		}
 		found=false;
-		for (int k = i, l = j ; k >=0 && l <8 && !found; k--, l++){
-			if( chess[k][l] != 'B' && chess[i][k] != 'Q' && chess[k][l] == '.')
+		for (int k = i, l = j ; k >=0 && l<8 && !found; k--, l++){
+			if( chess[k][l] != 'B' && chess[k][l] != 'Q' && chess[k][l] != '.' && chess[k][l] != 'k')
 				found = true;
-			if( chess[k][l] == 'B' || chess[i][k] == 'Q' ){
+			if( chess[k][l] == 'B' || chess[k][l] == 'Q' )
 				inCheck =  true;
-			}
 		}
 		found = false;
 		for (int k = i, l = j ; k <8 && l>=0  && !found; k++, l--){
-			if( chess[k][l] != 'B' && chess[i][k] != 'Q' && chess[k][l] == '.')
+			if( chess[k][l] != 'B' && chess[k][l] != 'Q' && chess[k][l] != '.' && chess[k][l] != 'k')
 				found = true;
-			if( chess[k][l] == 'B' || chess[i][k] == 'Q' ) {
+			if( chess[k][l] == 'B' || chess[k][l] == 'Q' ) {
 				inCheck = true;
 			}
 		}
 		return inCheck;
 	}
 	
-	
-//	
-//	private static boolean whiteKing( ){
-//
-//		if( wki-2 >= 0 && wkj-1 >= 0 && chess[wki-2][wkj-1]=='n' )
-//			return true;
-//		if( wki-1 >= 0 && wkj-2 >= 0 && chess[wki-1][wkj-2]=='n' )
-//			return true;
-//		if( wki+1 <  8 && wkj-2 >= 0 && chess[wki+1][wkj-2]=='n' )
-//			return true;
-//		if( wki+2 <  8 && wkj-1 >= 0 && chess[wki+2][wkj-1]=='n' )
-//			return true;
-//		if( wki-2 >= 0 && wkj+1 <  8  && chess[wki-2][wkj+1]=='n')
-//			return true;
-//		if( wki-1 >= 0 && wkj+2 <  8  && chess[wki-1][wkj+2]=='n')
-//			return true;
-//		if( wki+1 <  8 && wkj+2 <  8  && chess[wki+1][wkj+2]=='n')
-//			return true;
-//		if( wki+2 <  8 && wkj+1 <  8  && chess[wki+2][wkj+1]=='n')
-//			return true;
-//		
-//		if( (wki-1)>=0 && wkj-1>=0 && ( chess[wki-1][wkj-1] == 'q' || chess[wki-1][wkj-1] == 'p' ) )
-//			return true;
-//		if( wkj-1 >=0 && chess[wki][wkj-1] == 'q')
-//			return true;
-//		if( wki+1<8 && wkj-1 >=0 && chess[wki+1][wkj-1] == 'q')
-//			return true;
-//		if( wki+1<8 && chess[wki+1][wkj] == 'q')
-//			return true;
-//		if( wki+1<8 && wkj+1 < 8 && chess[wki+1][wkj+1] == 'q')
-//			return true;
-//		if( wkj+1 <8 && chess[wki][wkj+1] == 'q')
-//			return true;
-//		if( wki-1>=0 && wkj+1 <8 && ( chess[wki-1][wkj+1] == 'p' || chess[wki-1][wkj+1] == 'q') )
-//			return true;
-//		if( wki-1 >=0 && chess[wki-1][wkj] == 'q')
-//			return true;
-//		
-//		for( int i = wki, j = wkj; i>=0 && j<8 ; i--,j++ )
-//			if( chess[i][j] == 'b' || chess[i][j] == 'q' )
-//				return true;
-//		for( int i = wki, j = wkj; i<8 && j>=0 ; i++,j-- )
-//			if( chess[i][j] == 'b' || chess[i][j] == 'q' )
-//				return true;
-//		for( int i = wki, j = wkj; i>=0 && j>=0; i--,j-- )
-//			if( chess[i][j] == 'b' || chess[i][j] == 'q' )
-//				return true;
-//		for( int i = wki, j = wkj; i<8 && j<8 ; i++,j++ )
-//			if( chess[i][j] == 'b' || chess[i][j] == 'q' )
-//				return true;
-//		
-//		for(int i = wki; i>=0; i--)
-//			if( chess[i][wkj] == 'r' || chess[i][wkj] == 'q' )
-//				return true;
-//		for(int i = wki; i<8; i++)
-//			if( chess[i][wkj] == 'r' || chess[i][wkj] == 'q' )
-//				return true;
-//		for(int j = wkj; j>=0; j--)
-//			if( chess[wki][j] == 'r' || chess[wki][j] == 'q' )
-//				return true;
-//		for(int j = wkj; j<8; j++)
-//			if( chess[wki][j] == 'r' || chess[wki][j] == 'q' )
-//				return true;
-//		
-//		return false;
-//	}
-//	
-//	private static boolean blackKing( ){
-//		if( bki-2 >= 0 && bkj-1 >= 0  && chess[bki-2][bkj-1]=='N' )
-//			return true;
-//		if( bki-1 >= 0 && bkj-2 >= 0  && chess[bki-1][bkj-2]=='N' )
-//			return true;
-//		if( bki+1 <  8 && bkj-2 >= 0  && chess[bki+1][bkj-2]=='N' )
-//			return true;
-//		if( bki+2 <  8 && bkj-1 >= 0  && chess[bki+2][bkj-1]=='N' )
-//			return true;
-//		if( bki-2 >= 0 && bkj+1 <  8  && chess[bki-2][bkj+1]=='N')
-//			return true;
-//		if( bki-1 >= 0 && bkj+2 <  8  && chess[bki-1][bkj+2]=='N')
-//			return true;
-//		if( bki+1 <  8 && bkj+2 <  8  && chess[bki+1][bkj+2]=='N')
-//			return true;
-//		if( bki+2 <  8 && bkj+1 <  8  && chess[bki+2][bkj+1]=='N')
-//			return true;
-//		
-//		if( bki-1>=0 && bkj-1 >=0 && chess[bki-1][bkj-1] == 'Q')
-//			return true;
-//		if( bkj-1 >=0 && chess[bki][bkj-1] == 'Q')
-//			return true;
-//		if( bki+1<8 && bkj-1 >=0 && (chess[bki+1][bkj-1] == 'P' || chess[bki+1][bkj-1] == 'Q'))
-//			return true;
-//		if( bki+1<8 && chess[bki+1][bkj] == 'Q')
-//			return true;
-//		if( bki+1<8 && bkj+1 < 8 && (chess[bki+1][bkj+1] == 'P' || chess[bki+1][bkj+1] == 'Q'))
-//			return true;
-//		if( bkj+1<8 && chess[bki][bkj+1] == 'Q')
-//			return true;
-//		if( bki-1>=0 && bkj+1 <8 && chess[bki-1][bkj+1] == 'Q')
-//			return true;
-//		if( bki-1 >=0 && chess[bki-1][bkj] == 'Q')
-//			return true;
-//		
-//		for( int i = bki, j = bkj; i>=0 && j<8 ; i--,j++ )
-//			if( chess[i][j] == 'B' || chess[i][j] == 'Q' )
-//				return true;
-//		for( int i = bki, j = bkj; i<8 && j>=0 ; i++,j-- )
-//			if( chess[i][j] == 'B' || chess[i][j] == 'Q' )
-//				return true;
-//		for( int i = bki, j = bkj; i>=0 && j>=0; i--,j-- )
-//			if( chess[i][j] == 'B' || chess[i][j] == 'Q' )
-//				return true;
-//		for( int i = bki, j = bkj; i<8 && j<8 ; i++,j++ )
-//			if( chess[i][j] == 'B' || chess[i][j] == 'Q' )
-//				return true;
-//		
-//		for(int i = bki; i>=0; i--)
-//			if( chess[i][bkj] == 'R' || chess[i][bkj] == 'Q' )
-//				return true;
-//		for(int i = bki; i<8; i++)
-//			if( chess[i][bkj] == 'R' || chess[i][bkj] == 'Q' )
-//				return true;
-//		for(int j = bki; j>=0; j--)
-//			if( chess[bki][j] == 'R' || chess[bki][j] == 'Q' )
-//				return true;
-//		for(int j = bki; j<8; j++)
-//			if( chess[bki][j] == 'R' || chess[bki][j] == 'Q' )
-//				return true;
-//		
-//		return false;
-//	}
-	
-	private static void print( ){
-		for (int i = 0; i < chess.length; i++) {
-			for (int j = 0; j < chess[0].length; j++) {
-				System.out.print( chess[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-
 	private static boolean isEnd( ){
 		boolean end = true;
 		for (int i = 0; i < chess.length && end; i++) {
