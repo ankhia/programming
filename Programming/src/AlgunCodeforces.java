4e96585b7347a0b1fe9799b9022b3e35e17a2106
Milena Vega <milena.vega@od1.co>
2014-08-20 10:28:58 -0500

    A

--- Programming/src/AlgunCodeforces.java
+++ Programming/src/AlgunCodeforces.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-public class A {
+public class AlgunCodeforces {
 
 	public static void main(String[] args) throws Throwable{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +20,4 @@ public class A {
 		sb.append(cant+"\n");
 		System.out.print(new String(sb));
 	}
-
-
 }
