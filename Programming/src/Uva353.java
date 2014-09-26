ecbc7772fae9ff702e856365c30be15aa5e756b3
Milena <Milena@MilenaV>
2014-09-26 08:24:37 -0500

    fdsdf 353

---
+++ Programming/src/Uva353.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+
+public class Uva353 {
+
+	static TreeSet<String> pal;
+	public static void main(String[] args) throws Throwable {
+		BufferedReader in = new BufferedReader( new InputStreamReader(System.in) );
+		StringBuilder sb = new StringBuilder( );
+		for (String ln ; (ln = in.readLine())!=null; ) {
+			StringTokenizer st = new StringTokenizer(ln.trim());
+			String line = st.nextToken();
+			pal = new TreeSet<String>();
+			for (int i = 0; i < line.length(); i++) {
+				for (int j = i+1; j <= line.length(); j++) {
+					String n = line.substring(i, j);
+					String rev = new String(new StringBuilder(n).reverse());
+					if( n.equals(rev ))
+						pal.add(n);
+				}
+			}
+			if( pal.size() == 1 )
+				sb.append("The string '"+line+"' contains "+pal.size()+" palidrome.").append("\n");
+			else
+				sb.append("The string '"+line+"' contains "+pal.size()+" palidromes.").append("\n");
+		}
+		System.out.print(new String(sb));
+	}
+}
